8ca9e44663a0d2ae0fbe0b48a7dd7139edf86b5a
kotobema <kotobema@gmail.com>
2021-03-15 04:08:29 +0300

    commit slick slider

---
+++ src/main/resources/static/css/slick/slick-custom.js
@@ -0,0 +1,38 @@
+$(document).on('ready', function () {
+    $(".zoom").slick({
+        // lazyLoad: 'ondemand', // ondemand progressive anticipated
+      /*
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3500,
+        arrows: false,
+        respondTo: 'slider',
+        fade: true,
+        cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+        infinite: true,
+        draggable: true,*/
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 3500,
+        consoledraggable: true,
+        arrows: false,
+        fade: true,
+        speed: 900,
+        infinite: true,
+        cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+        touchThreshold: 100,
+    });
+    $(".variable").slick({
+        // lazyLoad: 'ondemand', // ondemand progressive anticipated
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3500,
+        arrows: false,
+        respondTo: 'slider',
+        fade: true,
+        infinite: true,
+        draggable: true,
+    });
+});
