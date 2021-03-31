4982ae5e7339d5be239f12ed4c762b6d584136c0
ulrich <ulrichandrianarivony@gmail.com>
2021-03-31 18:16:51 +0300

    tracker

--- src/main/java/com/Giz/repository/TrackerRepository.java
+++ src/main/java/com/Giz/repository/TrackerRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.Giz.data.domain.Tracker;;
 
@@ -13,5 +15,5 @@ import com.Giz.data.domain.Tracker;;
 public interface TrackerRepository extends CrudRepository<Tracker, Long> {
 	
 	@Query(value = "SELECT * FROM Tracker where type_tracker=:type_tracker",  nativeQuery = true)
-	List<Tracker> ListTracker(String type_tracker);
+	List<Tracker> ListTracker(@Param("type_tracker") String type_tracker);
 }
