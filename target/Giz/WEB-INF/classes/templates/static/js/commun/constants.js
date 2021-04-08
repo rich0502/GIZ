a961819b328ec5aada8d6b8e66dc0f57177b950d
ulrich <ulrichandrianarivony@gmail.com>
2021-04-08 20:55:14 +0300

    delete

--- target/Giz/WEB-INF/classes/templates/static/js/commun/constants.js
+++
@@ -1,8 +0,0 @@
-const constants = {
-	//adresseIP: "http://plan-etech.dev.arkeup.com",
-	//dns: "http://plan-etech.dev.arkeup.com",
-	adresseIP: "http://localhost:8083",
-	dns: "http://localhost:8083",
-    mercator: new OpenLayers.Projection("EPSG:3857")
-};
-export default constants;
--- target/classes/static/js/commun/constants.js
+++
@@ -1,8 +0,0 @@
-const constants = {
-	adresseIP: "http://168.119.185.165:8080",
-	dns: "http://168.119.185.165:8080",
-	//adresseIP: "http://localhost:8083",
-	//dns: "http://localhost:8083",
-    mercator: new OpenLayers.Projection("EPSG:3857")
-};
-export default constants;
